acde063a35ef9b09c42e08ef51cc33047c0fd87e
Nicklas Nielsen <nicklas.n@nielsen.mail.dk>
2019-03-13 15:54:36 +0100

    movie classes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MovieCRUDComponent } from './movie-crud/movie-crud.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    MovieCRUDComponent,
+    MovieDetailsComponent,
+   
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movie-crud/movie-crud.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-movie-crud',
+  templateUrl: './movie-crud.component.html',
+  styleUrls: ['./movie-crud.component.scss']
+})
+export class MovieCRUDComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
